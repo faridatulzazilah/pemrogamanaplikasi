ccc399c0145c940926c3bca257f5fa30fba24639
faridatulzazilah <43023322+faridatulzazilah@users.noreply.github.com>
2018-09-06 16:30:08 +0700

    Add files via upload

---
+++ TUGAS ANDROID FRD/src/tugas/android/frd/TUGASANDROIDFRD.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.android.frd;
+
+/**
+ *
+ * @author THECOM KRAKSAAN
+ */
+public class TUGASANDROIDFRD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jml = 5;
+        for (int i=0;i<jml;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");       
+        }
+        System.out.println(" ");
+        }
+    }
+}
